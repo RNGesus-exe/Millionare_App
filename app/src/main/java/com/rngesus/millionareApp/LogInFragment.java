b517ca5424dd70136e5b618a2ac7111835c673f6
rngesus <behzadshaheryar@gmail.com>
2020-12-20 18:58:32 +0500

    Added LogIn Fragment.

---
+++ app/src/main/java/com/rngesus/millionareApp/LogInFragment.java
@@ -0,0 +1,14 @@
+package com.rngesus.millionareApp;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+
+public class LogInFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
